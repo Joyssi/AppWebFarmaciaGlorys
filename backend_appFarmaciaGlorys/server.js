8550408e1cb07f8b8f14fdd61a169e6d33ad3125
Joy <izaduartes2214@gmail.com>
2023-10-06 18:57:35 -0600

    Modificación del archivo server.js

--- backend_appFarmaciaGlorys/server.js
+++ backend_appFarmaciaGlorys/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'gitdev2051',
+    password: 'dayana2005',
     database: 'farmaciaglorys'
 });
 
